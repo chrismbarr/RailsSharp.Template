af2f71ad29f3823c0b86ef949c132c59b3a3611c
Mitch Ferrer <mr.ferfer@gmail.com>
2015-02-01 20:45:38 -0600

    updating the fw components

--- RailsSharp.Backend/UserRoleHandlerBase.cs
+++ RailsSharp.Backend/UserRoleHandlerBase.cs
@@ -5,7 +5,7 @@ namespace RailsSharp.Backend
 	public abstract class UserRoleHandlerBase<TReq> : HandlerBase<TReq, ICustomPrinciple> where TReq : RequestBase
 	{
 		//rails# 3b) We can however make reusable authentication "contexts" for the handlers
-		protected override bool IsAuthorized(ICustomPrinciple principal)
+		protected override bool IsAuthorized(TReq request, ICustomPrinciple principal)
 		{
 			return principal.IsInRole(RoleEnum.User);
 		}
